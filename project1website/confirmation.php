306c3a4fb3354836fe242c2e941f945ffea54cc9
billy needrith <66972176+billy-need@users.noreply.github.com>
2020-10-09 15:47:58 -0400

    Add files via upload

--- project1website/confirmation.php
+++ project1website/confirmation.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <!-- Metadata -->
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" contect="chrome">
+        <meta http-equiv="X-UA-Compatible" contect="IE=edge">
         <meta name="viewport" content="width = device-width, initial-scale = 1">
 
         <!-- Title in tab -->
-        <title>Americano Pie Bar</title>
+        <title>Americano pie bar</title>
 
         <!-- Bootstrap 4.0 CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        
+
         <!-- Custom CSS -->
         <link rel="stylesheet" href="css/style.css">
 
@@ -19,8 +19,7 @@
         <link href="images/favicon.ico" rel="icon" type="image/x-icon" />
 
     </head>
-
-    <body>
+    <body onload="confirmationLoad()">
         <!-- Navigation bar -->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <!-- Logo -->
@@ -33,61 +32,43 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-                        <!-- Navigation Links -->
-                        <div class="collapse navbar-collapse" id="navbarLinks">
-                            <ul class="navbar-nav mr-auto">
-                                <li class="nav-item">
-                                    <a href="index.html" aria-haspopup="true" class="nav-link">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="menu.html" aria-haspopup="true" class="nav-link">Menu</a>
-                                </li>
-                                <li class="nav-item active">
-                                    <a href="reservation.html" aria-haspopup="true" class="nav-link">Reservation</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
+            <!-- Navigation Links -->
+            <div class="collapse navbar-collapse" id="navbarLinks">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a href="index.html" aria-haspopup="true" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="menu.html" aria-haspopup="true" class="nav-link">Menu</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a href="reservation.html" aria-haspopup="true" class="nav-link">Reservation</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
         <div class="container">
-
-            <!-- Page Header -->
-            <div class="row justify-content-center">
-                <header><h1>Our Menu</h1></header>
-            </div>
-            <hr>
-            <br>
-
-            <!-- Menu Container First Row -->
-            <div class="row justify-content-around">
-                <div class="menuCol col-4 col-sm-6">
-                    <img class="menuImg" src="images/upside down.jpg" alt="upside down pizza">
-                    <h4>Upside Down Pizza - $12.97</h4>
-                    <p>Fresh mozzarella, marinara.</p>
+            <div class="row">
+                <div class="col">
+                    <div class="jumbotron">
+                        <Header><h2 class="display-4">Rerservation has been confirmed!</h2></Header>
+                        <p class="lead">Congratulations <span class="bold"><?php echo $_GET["firstName"]; ?> <?php echo $_GET["lastName"]; ?></span>, your reservation for <span class="bold"><?php echo $_GET["partySelect"]; ?></span> people on <span class="bold"><?php echo $_GET["resDate"]; ?></span> at <span class="bold"><?php echo $_GET["resTime"]; ?></span> has been set.</p>
+                        <hr class="my-4">
+                        <p>A confirmation email was sent to <b><?php echo $_GET["email"]; ?></b>. Please arrive 15 minutes prior to your reservation time.</p>
+                        <div id="resOptions">
+                            <p ><h5>Requests: </h5>
+                                <div id="wheelchairCheck"><?php echo $_GET["wheelchairCheck"]; ?></div>
+                                <div id="highchairCheck"><?php echo $_GET["highchairCheck"]; ?></div>
+                                <div id="strollerCheck"><?php echo $_GET["strollerCheck"]; ?></div>
+                                <div id="outdoorCheck"><?php echo $_GET["outdoorCheck"]; ?></div>
+                            </p>
+                        </div>
+                        <div id="specText">
+                        <p><h5>Special Instructions: </h5><i><div id="specInstructionText"><?php echo $_GET["specInstructionText"]; ?></div></i></p>
+                        </div>
+                    </div>
                 </div>
-                <div class="menuCol col-4 col-sm-6">
-                    <img class="menuImg" src="images/shrimp fetta.jpg" alt="Shrimp Fettucine Alfredo">
-                    <h4>Fettuccine Americano- $19.95</h4>
-                    <p>Crab, shrimp, spinach, cognac, pink sauce.</p>
-                </div>
-            </div>
-
-            <!-- Menu Container Second Row -->
-            <div class="row justify-content-around">
-                <div class="menuCol col-4 col-sm-6">
-                    <img class="menuImg" src="images/spinach&artichoke pizza.jpg" alt="spinach & artichoke dip pizza">
-                    <h4>spinach & artichoke dip pizza - $14.00</h4>
-                    <p>Four cheese, spinach, artichokes.</p>
-                </div>
-                <div class="menuCol col-4 col-sm-6">
-                    <img class="menuImg" src="images/short rib mac.jpg" alt="Short Rib Mac">
-                    <h4>Short Rib Mac - $14.99</h4>
-                    <p>Three cheese, breadcrumbs, pulled short rib.</p>
-                </div>
-            </div>
-
-            <div class="row justify-content-center">
-                <header><h1>Many other options also available </h1></header>
             </div>
 
             <!-- Footer Menu -->
@@ -126,7 +107,7 @@
                                     517 Main St, Islip, NY 11751
                                 </p>
                                 <br>
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.4820932668513!2d-73.21596208488823!3d40.7294163444874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e833a4584014f9%3A0xe149a4c38450cd8f!2sAmericano%20Pie%20Bar!5e0!3m2!1sen!2sus!4v1602271661194!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>">
+                                <iframe src = "https://goo.gl/maps/8Y3ovmzPai4PwZ2f6"></iframe>
                             </div>
                         </div>
     
